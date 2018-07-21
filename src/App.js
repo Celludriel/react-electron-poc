eab7695909ae5d4f10d34829959dd5b3cb1d78a1
Kenny De Smedt <de_smedt_kenny@telenet.be>
2018-07-21 22:25:21 +0200

    - File load and display of data implementation - Removal of serviceworker (there is not enough information on the internet about serviceworker and create-react app + electron, when you create a distribution it can't find the service-worker.js file presumably because webpack has made it and thinks it's hosted from a webserver, which we don't have)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,45 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const electron = window.require('electron');
+const fs = electron.remote.require('fs');
+const app = electron.remote.app;
+
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            files: [],
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        const appPath = app.getAppPath();
+        console.log(appPath);
+        const path = app.getPath('userData');
+        console.log(path);
+        fs.readdir(appPath + '/data', (err, files) => {
+            this.setState({files: files});
+        });
+
+        const data = JSON.parse(fs.readFileSync(appPath + '/data/data.json', 'utf-8'));
+        this.setState({
+            ...this.state,
+            'data': data
+        });
+    }
+
+    renderFiles = () => {
+        return this.state.files.map((file, index) => {
+            return (<p key={index}>{file}</p>);
+        })
+    }
+
+    renderData = () => {
+        return JSON.stringify(this.state.data.slice(0,10));
+    }
 
     render() {
         return (<div className="App">
@@ -14,6 +52,10 @@ class App extends Component {
                 To get started, edit
                 <code>src/App.js</code>
                 and save to reload.
+            </p>
+            {this.renderFiles()}
+            <p>
+                {this.renderData()}
             </p>
         </div>);
     }
