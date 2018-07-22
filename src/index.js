4efe55e7948622d360d430ae303810613838cf62
Kenny De Smedt <de_smedt_kenny@telenet.be>
2018-07-22 11:24:39 +0200

    setup material-ui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider as ReduxProvider } from "react-redux";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import './index.css';
 import App from './views/App';
 import configureStore from "./state/store";
@@ -9,7 +11,9 @@ const reduxStore = configureStore(window.REDUX_INITIAL_DATA);
 
 ReactDOM.render(
     <ReduxProvider store={reduxStore}>
-        <App/>
+        <MuiThemeProvider>            
+            <App/>
+        </MuiThemeProvider>
     </ReduxProvider>,
     document.getElementById('root')
 );
