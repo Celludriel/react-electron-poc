270375035ab288bdb584bdfe541f9e6f76de0530
Kenny De Smedt <de_smedt_kenny@telenet.be>
2018-07-22 10:22:32 +0200

    redux setup with use of the ducks project structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider as ReduxProvider } from "react-redux";
 import './index.css';
-import App from './App';
+import App from './views/App';
+import configureStore from "./state/store";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reduxStore = configureStore(window.REDUX_INITIAL_DATA);
+
+ReactDOM.render(
+    <ReduxProvider store={reduxStore}>
+        <App/>
+    </ReduxProvider>,
+    document.getElementById('root')
+);
