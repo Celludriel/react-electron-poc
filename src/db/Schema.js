f587a99946affdbb3c78a7b7aee61704da56da95
Kenny De Smedt <de_smedt_kenny@telenet.be>
2018-07-24 17:58:07 +0200

    first attempt on creating a complicated schema

--- src/db/Schema.js
+++ src/db/Schema.js
@@ -1,6 +1,6 @@
 export const schema = {
-  title: 'Anonymous chat schema',
-  description: 'Database schema for an anonymous chat',
+  title: 'Card schema',
+  description: 'Database schema for a pokemon card',
   version: 0,
   type: 'object',
   properties: {
@@ -8,9 +8,136 @@ export const schema = {
       type: 'string',
       primary: true
     },
-    message: {
+    name: {
       type: 'string'
+    },
+    nationalPokedexNumber: {
+      type: 'string'
+    },
+    imageUrl: {
+      type: 'string'
+    },
+    imageUrlHiRes: {
+      type: 'string'
+    },
+    "types": {
+        "type": "array",
+        "items": {
+            "type": "string"
+        }
+    },
+    supertype: {
+      type: 'string'
+    },
+    subtype: {
+      type: 'string'
+    },
+    "ability": {
+      "type": "object",
+      "properties": {
+        "name": {
+            "type": "string"
+        },
+        "text": {
+            "type": "string"
+        },
+        "type": {
+            "type": "string"
+        }
+      }
+    },
+    "hp": {
+      "type": "string"
+    },
+    "retreatCost": {
+        "type": "array",
+        "items": {
+            "type": "string"
+        }
+    },
+    "convertedRetreatCost": {
+        "type": "integer"
+    },
+    "number": {
+        "type": "string"
+    },
+    "artist": {
+        "type": "string"
+    },
+    "rarity": {
+        "type": "string"
+    },
+    "series": {
+        "type": "string"
+    },
+    "set": {
+        "type": "string"
+    },
+    "setCode": {
+        "type": "string"
+    },
+    "text": {
+        "type": "array",
+        "items": {
+            "type": "string"
+        }
+    },
+    "attacks": {
+        "type": "array",
+        "items": {
+            "type": "object",
+            "properties": {
+                "cost": {
+                    "type": "array",
+                    "items": {
+                        "type": "string"
+                    }
+                },
+                "name": {
+                    "type": "string"
+                },
+                "text": {
+                    "type": "string"
+                },
+                "damage": {
+                    "type": "string"
+                },
+                "convertedEnergyCost": {
+                    "type": "integer"
+                }
+            },
+        }
+    },
+    "weaknesses": {
+        "type": "array",
+        "items": {
+            "type": "object",
+            "properties": {
+                "type": {
+                    "type": "string"
+                },
+                "value": {
+                    "type": "string"
+                }
+            }
+        }
+    },
+    "resistances": {
+        "type": "array",
+        "items": {
+            "type": "object",
+            "properties": {
+                "type": {
+                    "type": "string"
+                },
+                "value": {
+                    "type": "string"
+                }
+            }
+        }
+    },
+    "evolvesFrom": {
+        "type": "string"
     }
-  },
-  required: ['message']
+  }
 }
